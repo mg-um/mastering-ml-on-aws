afc6e5fce81ef8f9814a39fe11617794e84eeadf
mgurmendez <mgurmendez@dataxu.com>
2019-03-23 15:36:14 -0400

    reformatting

--- chapter8/tf_train.py
+++ chapter8/tf_train.py
@@ -3,7 +3,7 @@ import argparse
 import os
 import tensorflow as tf
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--epochs', type=int, default=100)
     parser.add_argument('--batch_size', type=int, default=1)
@@ -16,11 +16,11 @@ if __name__ =='__main__':
     housing_df = pd.read_csv(args.train + '/train.csv')
     training_features = ['crim', 'zn', 'indus', 'chas', 'nox', 'rm', 'age', 'dis', 'tax', 'ptratio', 'lstat']
     label = 'medv'
-    training_columns = [tf.feature_column.numeric_column(f) for f in training_features]
-    tf_regressor = tf.estimator.LinearRegressor(feature_columns=[tf.feature_column.numeric_column('inputs', shape=(11,))])  
+    tf_regressor = tf.estimator.LinearRegressor(
+        feature_columns=[tf.feature_column.numeric_column('inputs', shape=(11,))])
     training_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x = {'inputs':training_df[training_features].as_matrix()},
-        y = training_df[label].as_matrix(),
+        x={'inputs': housing_df[training_features].as_matrix()},
+        y=housing_df[label].as_matrix(),
         shuffle=False,
         batch_size=args.batch_size,
         num_epochs=args.epochs,
@@ -28,8 +28,11 @@ if __name__ =='__main__':
         num_threads=1)
     tf_regressor.train(input_fn=training_input_fn, steps=args.steps)
 
+
     def serving_input_fn():
         feature_spec = tf.placeholder(tf.float32, shape=[1, 11])
-        return tf.estimator.export.build_parsing_serving_input_receiver_fn({'input': tensor})()
+        return tf.estimator.export.build_parsing_serving_input_receiver_fn({'input': feature_spec})()
 
-    tf_regressor.export_savedmodel(export_dir_base = args.local_model_dir + '/export/Servo', serving_input_receiver_fn = serving_input_fn)
+
+    tf_regressor.export_savedmodel(export_dir_base=args.local_model_dir + '/export/Servo',
+                                   serving_input_receiver_fn=serving_input_fn)
